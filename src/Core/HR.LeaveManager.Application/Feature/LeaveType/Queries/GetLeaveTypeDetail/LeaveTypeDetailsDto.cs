0741baeeabaf7d0029ea4ad7d9705a021f13e69b
vik37 <vik.zafirovski@gmail.com>
2025-02-20 20:45:06 +0100

    Additional

--- src/Core/HR.LeaveManager.Application/Feature/LeaveType/Queries/GetLeaveTypeDetail/LeaveTypeDetailsDto.cs
+++ src/Core/HR.LeaveManager.Application/Feature/LeaveType/Queries/GetLeaveTypeDetail/LeaveTypeDetailsDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace HR.LeaveManager.Application.Feature.LeaveType.Queries.GetLeaveTypeDetail;
 
-public class LeaveTypeDetailDto
+public class LeaveTypeDetailsDto
 {
 	public int Id { get; set; }
 	public string Name { get; set; } = string.Empty;
