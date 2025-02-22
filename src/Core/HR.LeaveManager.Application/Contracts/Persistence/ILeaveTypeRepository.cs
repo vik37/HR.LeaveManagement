47417a05cf5e6bd885cf215946b13e6f559b96a0
vik37 <vik.zafirovski@gmail.com>
2025-02-22 22:31:03 +0100

    [setup-fluent-validation] Leave Type Fluent Validator

--- src/Core/HR.LeaveManager.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ src/Core/HR.LeaveManager.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -4,4 +4,5 @@ namespace HR.LeaveManager.Application.Contracts.Persistence;
 
 public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
 {
+	Task<bool> IsLeaveTypeUnique(string name);
 }
