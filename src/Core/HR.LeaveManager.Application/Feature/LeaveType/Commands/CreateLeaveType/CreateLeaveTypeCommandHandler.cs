1dd8e42b72eff31b9fe028a21609c5bd5b093904
vik37 <vik.zafirovski@gmail.com>
2025-02-27 21:07:27 +0100

    [logger-implementation] Implementation of Logging tracks and trace errors

--- src/Core/HR.LeaveManager.Application/Feature/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
+++ src/Core/HR.LeaveManager.Application/Feature/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -2,6 +2,7 @@
 using HR.LeaveManager.Application.Exceptions;
 using HR.LeaveManager.Application.Contracts.Persistence;
 using MediatR;
+using HR.LeaveManager.Application.Contracts.Logging;
 
 namespace HR.LeaveManager.Application.Feature.LeaveType.Commands.CreateLeaveType;
 
@@ -9,11 +10,14 @@ public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeComm
 {
 	private readonly IMapper _mapper;
 	private readonly ILeaveTypeRepository _leaveTypeRepository;
+	private readonly IAppLogger<CreateLeaveTypeCommandHandler> _logger;
 
-	public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+	public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository,
+		IAppLogger<CreateLeaveTypeCommandHandler> logger)
 	{
 		_mapper = mapper;
 		_leaveTypeRepository = leaveTypeRepository;
+		_logger = logger;
 	}
 
 	public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
@@ -23,13 +27,19 @@ public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeComm
 		var validatorResult = await validator.ValidateAsync(request);
 
 		if (!validatorResult.IsValid)
+		{
+			_logger.LogWarning("Validation errors in requests for creation new {0}", nameof(LeaveType));
+
 			throw new BadRequestException("Invalid Leave Type", validatorResult);
+		}
+			
 		// Convert to domain object
 		var leaveTypeCreated = _mapper.Map<LeaveManagement.Domain.LeaveType>(request);
 
 		// Add to database
 		await _leaveTypeRepository.CreateAsync(leaveTypeCreated);
 
+		_logger.LogInformation("{0} was successfully created.", nameof(LeaveType));
 		// return record id
 		return leaveTypeCreated.Id;
 	}
--- src/Core/HR.LeaveManager.Application/Feature/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommaindHandler.cs
+++ src/Core/HR.LeaveManager.Application/Feature/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommaindHandler.cs
@@ -1,16 +1,20 @@
 ﻿using HR.LeaveManager.Application.Exceptions;
 using HR.LeaveManager.Application.Contracts.Persistence;
 using MediatR;
+using HR.LeaveManager.Application.Contracts.Logging;
 
 namespace HR.LeaveManager.Application.Feature.LeaveType.Commands.DeleteLeaveType;
 
 public class DeleteLeaveTypeCommaindHandler : IRequestHandler<DeleteLeaveTypeCommaind, Unit>
 {
 	private readonly ILeaveTypeRepository _leaveTypeRepository;
+	private readonly IAppLogger<DeleteLeaveTypeCommaindHandler> _logger;
 
-	public DeleteLeaveTypeCommaindHandler(ILeaveTypeRepository leaveTypeRepository)
+	public DeleteLeaveTypeCommaindHandler(ILeaveTypeRepository leaveTypeRepository,
+		IAppLogger<DeleteLeaveTypeCommaindHandler> logger)
 	{
 		_leaveTypeRepository = leaveTypeRepository;
+		_logger = logger;
 	}
 
 	public async Task<Unit> Handle(DeleteLeaveTypeCommaind request, CancellationToken cancellationToken)
@@ -18,10 +22,15 @@ public class DeleteLeaveTypeCommaindHandler : IRequestHandler<DeleteLeaveTypeCom
 		var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
 
 		if (leaveTypeToDelete is null)
+		{
+			_logger.LogWarning("Invalid {0} - {1}", nameof(LeaveType)
+				, request.Id);
 			throw new NotFoundException(nameof(LeaveType), request.Id);
+		}
 
 		await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
 
+		_logger.LogInformation("{0} with id {1} was removed successfully.", nameof(LeaveType), request.Id);
 		return Unit.Value;
 	}
 }
