a5051d43771b29b498af99a13dc99a04327897ec
vik37 <vik.zafirovski@gmail.com>
2025-03-08 21:05:21 +0100

    [leave-allocation-complete-api-feature] Completing Leave Architecture Feature

---
+++ src/API/HR.LeaveManagement.Api/Controllers/LeaveAllocationController.cs
@@ -0,0 +1,76 @@
+﻿using HR.LeaveManager.Application.Feature.LeaveAllocation.Commands.CreateLeaveAllocation;
+using HR.LeaveManager.Application.Feature.LeaveAllocation.Commands.DeleteLeaveAllocation;
+using HR.LeaveManager.Application.Feature.LeaveAllocation.Commands.UpdateLeaveAllocation;
+using HR.LeaveManager.Application.Feature.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HR.LeaveManager.Application.Feature.LeaveAllocation.Queries.GetLeaveAllocations;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveAllocationController : ControllerBase
+    {
+		private readonly IMediator _mediator;
+
+		public LeaveAllocationController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		// GET: api/<LeaveAllocationController>
+		[HttpGet]
+        public async Task<ActionResult<List<LeaveAllocationDto>>> Get()
+        {
+            var leaveAllocations = await _mediator.Send(new GetLeaveAllocationListQuery());
+
+            return Ok(leaveAllocations);
+        }
+
+        // GET api/<LeaveAllocationController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveAllocationDetailsDto>> Get(int id)
+        {
+            var leaveAllocationDetails = await _mediator.Send(new LeaveAllocationDelailsQuery(id));
+
+            return Ok(leaveAllocationDetails);
+        }
+
+        // POST api/<LeaveAllocationController>
+        [HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> Post([FromBody] CreateLeaveAllocationCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return CreatedAtAction(nameof(Get), new { Id = response });
+        }
+
+        // PUT api/<LeaveAllocationController>/5
+        [HttpPut("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> Put(int id, [FromBody]UpdateLeaveAllocationCommand command)
+        {
+			var response = await _mediator.Send(command);
+			return NoContent();
+		}
+
+        // DELETE api/<LeaveAllocationController>/5
+        [HttpDelete("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteLeaveAllocationCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
---
+++ src/Core/HR.LeaveManager.Application/Feature/LeaveAllocation/Queries/GetLeaveAllocationDetails/LeaveAllocationDelailsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace HR.LeaveManager.Application.Feature.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+
+public record LeaveAllocationDelailsQuery(int Id) : IRequest<LeaveAllocationDetailsDto>;
