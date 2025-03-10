5ba5110aff6ba88d7fc9cc668e25220fca06cb9e
vik37 <vik.zafirovski@gmail.com>
2025-03-10 22:47:14 +0100

    [setup-unit-test] Setup XUnit Tests and Initial implementation of Leave Type Tests

--- src/Core/HR.LeaveManager.Application/Feature/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
+++ src/Core/HR.LeaveManager.Application/Feature/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -5,14 +5,14 @@ using MediatR;
 
 namespace HR.LeaveManager.Application.Feature.LeaveType.Queries.GetAllLeaveTypes;
 
-public class GetLeaveTypesHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
 {
 	private readonly IMapper _mapper;
 	private readonly ILeaveTypeRepository _leaveTypeRepository;
-	private readonly IAppLogger<GetLeaveTypesHandler> _logger;
+	private readonly IAppLogger<GetLeaveTypesQueryHandler> _logger;
 
-	public GetLeaveTypesHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository,
-		IAppLogger<GetLeaveTypesHandler> logger)
+	public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository,
+		IAppLogger<GetLeaveTypesQueryHandler> logger)
 	{
 		_mapper = mapper;
 		_leaveTypeRepository = leaveTypeRepository;
---
+++ test/HR.LeaveManagement.Application.UnitTest/Feature/LeaveTypes/Queries/GetLeaveTypeListsQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Castle.Core.Logging;
+using HR.LeaveManagement.Application.UnitTest.Mocs;
+using HR.LeaveManager.Application.Contracts.Logging;
+using HR.LeaveManager.Application.Contracts.Persistence;
+using HR.LeaveManager.Application.Feature.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManager.Application.Feature.LeaveType.Queries.GetLeaveTypeDetail;
+using HR.LeaveManager.Application.MappingProfiles;
+using Moq;
+using Shouldly;
+
+namespace HR.LeaveManagement.Application.UnitTest.Feature.LeaveTypes.Queries;
+
+public class GetLeaveTypeListsQueryHandlerTests
+{
+	private readonly Mock<ILeaveTypeRepository> _mockRepo;
+	private readonly IMapper _mapper;
+	private readonly Mock<IAppLogger<GetLeaveTypesQueryHandler>> _appLogger;
+
+	public GetLeaveTypeListsQueryHandlerTests()
+	{
+		_mockRepo = MockLeaveTypeRepository.GetLeaveTypesMockLeaveTypeRepository();
+
+		var mappConfig = new MapperConfiguration(c =>
+		{
+			c.AddProfile<LeaveTypeProfile>();
+		});
+
+		_mapper = mappConfig.CreateMapper();
+		_appLogger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+	}
+
+	[Fact]
+	public async Task GetLeaveTypeListTest()
+	{
+		var handler = new GetLeaveTypesQueryHandler(_mapper,_mockRepo.Object,_appLogger.Object);
+
+		var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+		result.ShouldBeOfType<List<LeaveTypeDto>>();
+		result.Count.ShouldBe(3);
+	}
+}
