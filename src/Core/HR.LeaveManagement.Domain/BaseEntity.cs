35d3025bd8c5049f9f6764344a6fd32723cbe428
vik37 <vik.zafirovski@gmail.com>
2025-03-12 16:15:59 +0100

    [setup-integration-test] Integration Tests for Entity Framework

--- src/Core/HR.LeaveManagement.Domain/BaseEntity.cs
+++ src/Core/HR.LeaveManagement.Domain/BaseEntity.cs
@@ -4,5 +4,5 @@ public class BaseEntity
 {
 	public int Id { get; set; }
 	public DateTime DateCreated { get; set; }
-	public DateTime DateModified { get; set; }
+	public DateTime? DateModified { get; set; }
 }
