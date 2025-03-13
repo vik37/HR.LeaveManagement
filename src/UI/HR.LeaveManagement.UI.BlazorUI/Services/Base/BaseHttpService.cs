462b968c97b3e927c31dbd14056ba2d715643384
vik37 <vik.zafirovski@gmail.com>
2025-03-13 20:21:38 +0100

    [(setup-blazor-webassembly] UI Blazor WebAssembly UI project

---
+++ src/UI/HR.LeaveManagement.UI.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.UI.BlazorUI.Services.Base;
+
+public class BaseHttpService
+{
+	protected  IClient _client;
+
+	public BaseHttpService(IClient client)
+	{
+		_client = client;
+	}
+}
